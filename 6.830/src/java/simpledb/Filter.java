5248708a35878351103f4901ac73c6302be07cec
Zeyuan Shang <zeyuanxy@gmail.com>
2015-11-01 15:29:54 -0500

    implement the Filter in lab2

--- 6.830/src/java/simpledb/Filter.java
+++ 6.830/src/java/simpledb/Filter.java
@@ -6,6 +6,9 @@ import java.util.*;
  */
 public class Filter extends Operator {
 
+    private Predicate predicate;
+    private DbIterator iterator;
+
     /**
      * Constructor accepts a predicate to apply and a child
      * operator to read tuples to filter from.
@@ -14,25 +17,25 @@ public class Filter extends Operator {
      * @param child The child operator
      */
     public Filter(Predicate p, DbIterator child) {
-        // some code goes here
+        predicate = p;
+        iterator = child;
     }
 
     public TupleDesc getTupleDesc() {
-        // some code goes here
-        return null;
+        return iterator.getTupleDesc();
     }
 
     public void open()
         throws DbException, NoSuchElementException, TransactionAbortedException {
-        // some code goes here
+        iterator.open();
     }
 
     public void close() {
-        // some code goes here
+        iterator.close();
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
-        // some code goes here
+        iterator.rewind();
     }
 
     /**
@@ -46,7 +49,12 @@ public class Filter extends Operator {
      */
     protected Tuple fetchNext()
         throws NoSuchElementException, TransactionAbortedException, DbException {
-        // some code goes here
+        while (iterator.hasNext()) {
+            Tuple tuple = iterator.next();
+            if (predicate.filter(tuple)) {
+                return tuple;
+            }
+        }
         return null;
     }
 }
