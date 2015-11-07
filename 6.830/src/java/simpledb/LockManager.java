0618be2ebd6d1bbab697344c920090908ec24b26
Zeyuan Shang <zeyuanxy@gmail.com>
2015-11-07 02:05:01 -0500

    implement the dependencyGraph to handle deadlock

--- 6.830/src/java/simpledb/LockManager.java
+++ 6.830/src/java/simpledb/LockManager.java
@@ -1,7 +1,9 @@
 package simpledb;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.HashMap;
+import java.util.Stack;
 import java.util.Collection;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -13,12 +15,14 @@ public class LockManager {
     private final HashMap<PageId, ArrayList<TransactionId>> sharedLocks;
     private final HashMap<PageId, TransactionId> exclusiveLocks;
     private final ConcurrentHashMap<TransactionId, Collection<PageId>> transactionPages;
+    private final ConcurrentHashMap<TransactionId, Collection<TransactionId>> dependencyGraph;
 
     private LockManager() {
         locks = new ConcurrentHashMap<PageId, Object>();
         sharedLocks = new HashMap<PageId, ArrayList<TransactionId>>();
         exclusiveLocks = new HashMap<PageId, TransactionId>();
         transactionPages = new ConcurrentHashMap<TransactionId, Collection<PageId>>();
+        dependencyGraph = new ConcurrentHashMap<TransactionId, Collection<TransactionId>>();
     }
 
     public static LockManager getInstance() {
@@ -40,13 +44,13 @@ public class LockManager {
         return locks.get(pageId);
     }
 
-    private void addPagetoTransactionPages(TransactionId transactionId,
+    private void addPageToTransactionPages(TransactionId transactionId,
                                            PageId pageId) {
         transactionPages.putIfAbsent(transactionId, new LinkedBlockingQueue<PageId>());
         transactionPages.get(transactionId).add(pageId);
     }
 
-    public boolean acquireLock(TransactionId transactionId, PageId pageId, Permissions permissions) {
+    public boolean acquireLock(TransactionId transactionId, PageId pageId, Permissions permissions) throws TransactionAbortedException {
         if (hasPermissions(transactionId, pageId, permissions)) {
             return true;
         }
@@ -59,11 +63,13 @@ public class LockManager {
                 synchronized (lock) {
                     TransactionId exclusiveLockHolder = exclusiveLocks.get(pageId);
                     if (exclusiveLockHolder == null || transactionId.equals(exclusiveLockHolder)) {
+                        removeDependencies(transactionId);
                         sharedLocks.putIfAbsent(pageId, new ArrayList<TransactionId>());
                         sharedLocks.get(pageId).add(transactionId);
-                        addPagetoTransactionPages(transactionId, pageId);
+                        addPageToTransactionPages(transactionId, pageId);
                         return true;
                     }
+                    addDependency(transactionId, exclusiveLockHolder);
                 }
             }
         } else {
@@ -77,9 +83,10 @@ public class LockManager {
                     }
                     if (lockHolders.isEmpty() || (lockHolders.size() == 1 && transactionId.equals(lockHolders.iterator().next()))) {
                         exclusiveLocks.put(pageId, transactionId);
-                        addPagetoTransactionPages(transactionId, pageId);
+                        addPageToTransactionPages(transactionId, pageId);
                         return true;
                     }
+                    addDependencies(transactionId, lockHolders);
                 }
             }
         }
@@ -124,4 +131,59 @@ public class LockManager {
         }
         return false;
     }
+
+    private void checkCycle(TransactionId transactionId, HashSet<TransactionId> testedTransactionIds, Stack<TransactionId> parents) throws TransactionAbortedException {
+        testedTransactionIds.add(transactionId);
+        if (dependencyGraph.containsKey(transactionId)) {
+            for (TransactionId dependee : dependencyGraph.get(transactionId)) {
+                if (parents.contains(dependee)) {
+                    throw new TransactionAbortedException();
+                }
+                if (testedTransactionIds.contains(dependee)) {
+                    continue;
+                }
+                parents.push(transactionId);
+                checkCycle(dependee, testedTransactionIds, parents);
+                parents.pop();
+            }
+        }
+    }
+
+    private void checkDeadLock() throws TransactionAbortedException {
+        HashSet<TransactionId> testedTransactionIds = new HashSet<TransactionId>();
+        for (TransactionId transactionId : dependencyGraph.keySet()) {
+            if (testedTransactionIds.contains(transactionId)) {
+                continue;
+            }
+            checkCycle(transactionId, testedTransactionIds, new Stack<TransactionId>());
+        }
+    }
+
+    private void addDependency(TransactionId dependent, TransactionId dependee) throws TransactionAbortedException {
+        Collection<TransactionId> dependees = new ArrayList<TransactionId>();
+        dependees.add(dependee);
+        addDependencies(dependent, dependees);
+    }
+
+    private void addDependencies(TransactionId dependent, Collection<TransactionId> dependees) throws TransactionAbortedException  {
+        dependencyGraph.putIfAbsent(dependent, new LinkedBlockingQueue<TransactionId>());
+        Collection<TransactionId> dependeesCollection = dependencyGraph.get(dependent);
+        boolean needCheck = false;
+        for (TransactionId dependee : dependees) {
+            if (!dependencyGraph.get(dependent).contains(dependee) && !dependee.equals(dependent)) {
+                needCheck = true;
+                dependeesCollection.add(dependee);
+            }
+        }
+        if (needCheck) {
+            checkDeadLock();
+        }
+    }
+
+
+    private void removeDependencies(TransactionId transactionId) {
+        dependencyGraph.remove(transactionId);
+    }
+
+
 }
