a4ecd48481ea248b27673c1e23169f6508dc2e81
Zeyuan Shang <zeyuanxy@gmail.com>
2015-11-06 22:54:39 -0500

    add a simple LockManager

---
+++ 6.830/src/java/simpledb/LockManager.java
@@ -0,0 +1,72 @@
+package simpledb;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class LockManager {
+
+    private final ConcurrentHashMap<PageId, Object> locks;
+    private final HashMap<PageId, ArrayList<TransactionId>> sharedLocks;
+    private final HashMap<PageId, TransactionId> exclusiveLocks;
+
+    private LockManager() {
+        locks = new ConcurrentHashMap<PageId, Object>();
+        sharedLocks = new HashMap<PageId, ArrayList<TransactionId>>();
+        exclusiveLocks = new HashMap<PageId, TransactionId>();
+    }
+
+    public static LockManager getInstance() {
+        return new LockManager();
+    }
+
+    private boolean hasPermissions(TransactionId transactionId, PageId pageId, Permissions permissions) {
+        if (exclusiveLocks.containsKey(pageId) && transactionId.equals(exclusiveLocks.get(pageId))) {
+            return true;
+        }
+        if (permissions == Permissions.READ_ONLY) {
+            return sharedLocks.containsKey(pageId) && sharedLocks.get(pageId).contains(transactionId);
+        }
+        return false;
+    }
+
+    private Object getLock(PageId pageId) {
+        locks.putIfAbsent(pageId, new Object());
+        return locks.get(pageId);
+    }
+
+    public boolean acquireLock(TransactionId transactionId, PageId pageId, Permissions permissions) {
+        if (hasPermissions(transactionId, pageId, permissions) {
+            return true;
+        }
+        Object lock = getLock(pageId);
+        if (permissions == Permissions.READ_ONLY) {
+            while (true) {
+                synchronized (lock) {
+                    TransactionId exclusiveLockHolder = exclusiveLocks.get(pageId);
+                    if (exclusiveLockHolder == null || transactionId.equals(exclusiveLockHolder)) {
+                        sharedLocks.putIfAbsent(pageId, new ArrayList<TransactionId>());
+                        sharedLocks.get(pageId).add(transactionId);
+                        return true;
+                    }
+                }
+            }
+        } else {
+            while (true) {
+                synchronized (lock) {
+                    ArrayList<TransactionId> lockHolders = new ArrayList<TransactionId>();
+                    if (exclusiveLocks.containsKey(pageId)) {
+                        lockHolders.add(exclusiveLocks.get(pageId));
+                    } else {
+                        lockHolders.addAll(sharedLocks.get(pageId));
+                    }
+                    if (lockHolders.isEmpty() || (lockHolders.size() == 1 && transactionId.equals(lockHolders.iterator().next()))) {
+                        exclusiveLocks.put(pageId, transactionId);
+                    }
+                }
+            }
+        }
+    }
+}
